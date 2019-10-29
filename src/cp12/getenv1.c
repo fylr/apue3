a43ded570132b82eb83783df29a74aa82e0481cd
fylr <cky126@qq.com>
2019-10-28 22:01:08 -0400

    [A] add cp12 dir

---
+++ src/cp12/getenv1.c
@@ -0,0 +1,32 @@
+#include <limits.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAXSTRINGSZ	4096
+
+static char envbuf[MAXSTRINGSZ];
+
+extern char **environ;
+
+char *
+getenv(const char *name)
+{
+	int i, len;
+
+	len = strlen(name);
+	for (i = 0; environ[i] != NULL; i++) {
+		if ((strncmp(name, environ[i], len) == 0) &&
+		  (environ[i][len] == '=')) {
+			strncpy(envbuf, &environ[i][len+1], MAXSTRINGSZ-1);
+			return(envbuf);
+		}
+	}
+	return(NULL);
+}
+
+int
+main(void)
+{
+	printf("PATH ==> %s\n", getenv("PATH"));
+	return 0;
+}
